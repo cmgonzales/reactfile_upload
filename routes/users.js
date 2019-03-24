eb780a8c581f34d444447da700c841ece26ddf2e
Christopher Gonzales <cm.gonzales@yahoo.com>
2019-03-24 16:55:40 -0700

    first commit

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+  const types = [
+    {id: 1, name: "samantha", color: "blue"},
+    {id: 2, name: "allysa", color: "yellow"},
+    {id: 3, name: "olivia", color: "orange"}
+  ]
+  res.send(types)
+});
+
+module.exports = router;
