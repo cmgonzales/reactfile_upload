85610fb2bab9109be71e02748084fd588ef008f8
Christopher Gonzales <cm.gonzales@yahoo.com>
2019-04-04 18:33:34 -0700

    added cors

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,7 +16,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,11 +25,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('./upload', fileRouter);
+app.use('/upload', fileRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+app.use(fileUpload())
+
+
+app.post('/upload', (req, res, next) => {
+    console.log(req);
+    let imageFile = req.files.file;
+  
+    imageFile.mv(`${__dirname}/public/${req.body.filename}.jpg`, function(err) {
+      if (err) {
+        return res.status(500).send(err);
+      }
+  
+      res.json({file: `public/${req.body.filename}.jpg`});
+    });
+  
+  })
 
 // error handler
 app.use(function(err, req, res, next) {
