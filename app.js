20b0e0b59b21f59f7596d5dc00fc35a3cf4cda03
Christopher Gonzales <cm.gonzales@yahoo.com>
2019-04-05 23:50:53 -0700

    deleted file

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var fileRouter = require('./routes/upload');
 
 
 var app = express();
@@ -25,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/upload', fileRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/upload.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-
-router.post('/', function(req, res, next) {
-  res.send("upload page");
-});
-
-module.exports = router
